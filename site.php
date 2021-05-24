e5cd47bbd4da1356fc2ecccd38c2e6fa55d7382b
Selma Haracic <selma.haracic@systech.ba>
2021-05-24 15:58:50 +0200

    first commit

---
+++ site.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo"<p>There once was a man named George <br></p>";
+        echo"<p>He was 70years old <br></p>";
+        echo"<p>He really liked the name George <br></p>";
+        echo"<p>but didn't like being 70 <br></p>";
+        ?>
+
+    </body>
+</html>
